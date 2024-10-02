812021e72970872ac89982c044e4237d94ddb70a
Md Istiaq Ahmed <mdistiaqahmed0@gmail.com>
2024-10-02 04:47:33 -0500

    Create LinkedStack.java

---
+++ Projects/LinkedStack.java
@@ -0,0 +1,23 @@
+
+public class LinkedStack<E> {
+    private Node<E> top;
+
+    public void push(E toPush) {
+        top = new Node<>(toPush, top);
+    }
+
+    public E pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        E value = top.value();
+        top = top.next();
+        return value;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    private record Node<E>(E value, Node<E> next) {}
+}
